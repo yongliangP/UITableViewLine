8344b20a100c661f9a44d1a2ef040529d6f9d68d
yongliangPeng <1994577317@qq.com>
2016-06-23 11:52:56 +0800

    Initial Commit

---
+++ UITableViewLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewLine
+//
+//  Created by 彭永亮 on 16/6/23.
+//  Copyright © 2016年 yongliangP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewLine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewLine
+//
+//  Created by 彭永亮 on 16/6/23.
+//  Copyright © 2016年 yongliangP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
